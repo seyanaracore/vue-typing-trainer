2fdedd62d4705a6e39faab566db13c4456b0c231
Anton Tigunov <seyanaracore@gmail.com>
2022-08-22 15:02:45 +0300

    feat: App layout, UI Components
    UI loader, button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="appContainer">
+      <header>
+        <h1>Тренажёр печати</h1>
+      </header>
+      <div v-if="isLoading" class="loaderContainer">
+        <loader />
+      </div>
+      <div v-else-if="fetchError">
+        <h3 class="text-danger py-2">Произошла ошибка...</h3>
+        <Button @onClick="fetchText">Попробовать снова</Button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
+import { LOCAL_TYPING_RESULTS_KEY } from './constants'
+import getText from '@/api/getText'
+import LocalStorageUtil from './utils/localStorageUtil'
+import Loader from './components/UI/UiLoader.vue'
+import Button from './components/UI/UiButton.vue'
 
 export default {
   name: 'App',
@@ -70,4 +88,36 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+
+  min-width: 800px;
+  min-height: 100vh;
+  padding: 0 12px;
+
+  background: rgb(180, 180, 182);
+  background: linear-gradient(
+    90deg,
+    rgba(180, 180, 182, 1) 0%,
+    rgba(22, 129, 122, 1) 56%,
+    rgba(60, 133, 99, 1) 100%
+  );
+}
+.appContainer {
+  width: 100%;
+  max-width: 1200px;
+  margin: 100px auto 12px;
+  padding: 24px;
+
+  background-color: white;
+  border: 1px solid #73ccfb;
+  border-radius: 25px;
+  box-shadow: rgb(60 64 67 / 30%) 0 1px 2px 0, rgb(60 64 67 / 15%) 0 2px 6px 2px;
+}
+.loaderContainer {
+  width: 100%;
+  text-align: center;
+}
+</style>
---
+++ src/components/UI/UiButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn" type="button" :class="colorType" @click="clickHandler">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'button-ui',
+  props: {
+    colorType: {
+      type: String,
+      default: 'btn-primary',
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.$emit('onClick')
+    },
+  },
+}
+</script>
+
+<style></style>
