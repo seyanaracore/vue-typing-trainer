356fdafac15a325e167f81dc5a8f5a73b1f30e9e
Anton Tigunov <seyanaracore@gmail.com>
2022-08-22 14:48:28 +0300

    feat: getting text and set state
    Fetch text from api and handling fetching stages

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,29 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default {
   name: 'App',
   components: {},
+  data() {
+    return {
+      textForTyping: '',
+      fetchError: false,
+      isLoading: false,
+      typingTextsHistoryList: [],
+    }
+  },
+  methods: {
+    async fetchText() {
+      this.isLoading = true
+      try {
+        const response = await getText()
+        const [text] = response
+        this.textForTyping = text
+      } catch (e) {
+        this.fetchError = true
+      }
+      this.isLoading = false
+    },
+  created() {
+    this.fetchText()
+  },
 }
 </script>
 
---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+class Api {
+  #request(url, method) {
+    return fetch(url, { method })
+  }
+
+  async get(url) {
+    const response = await this.#request(url, 'GET')
+    return response.json()
+
+    // try {
+    //   const response = await this.#request(url, 'GET')
+    //   return response.json()
+    // } catch (e) {
+    //   console.log('ошибка' + e)
+    //   console.error(e)
+    //   return e
+    // }
+  }
+}
+
+export default new Api()
